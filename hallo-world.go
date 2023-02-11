041a18bd1674f574db1df23b27b5ee24f26cce3f
Tugus <idabgusindra01@gmail.com>
2023-02-11 09:42:45 +0800

    add hello wolrd in go with goland

---
+++ hallo-world.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
+
+// build golang with "go build (fileName.go)"
+// runing while development without build you can use this command in terminal
+// go run (fileName.go)
